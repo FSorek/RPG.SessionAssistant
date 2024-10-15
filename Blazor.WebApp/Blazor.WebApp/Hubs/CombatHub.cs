65d4e0c943f40085226328feba707e80af2b8d2d
FSorek <filip.sorek96@gmail.com>
2024-10-15 21:32:24 +0200

    SignalR combat hub setup, MudBlazor UI basic layout

---
+++ Blazor.WebApp/Blazor.WebApp/Hubs/CombatHub.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Blazor.WebApp.Hubs;
+
+public class CombatHub : Hub
+{
+    private static List<(string user, string message)> _messageHistory = [];
+    
+    public async Task EnterCombat(int initiative)
+    {
+        await SendMessageToAllAsync(Context.ConnectionId, $"Joined combat with {initiative} initiative");
+    }
+
+    public override async Task OnConnectedAsync()
+    {
+        var messages = _messageHistory.Select(m => $"{m.user}: {m.message}").ToArray();
+        await Clients.Client(Context.ConnectionId).SendAsync("LoadMessages", messages);
+        await SendMessageToAllAsync(Context.ConnectionId, "Connected");
+    }
+
+    public override async Task OnDisconnectedAsync(Exception? exception)
+    {
+        if(exception == null)
+            await SendMessageToAllAsync(Context.ConnectionId, "Disconnected (Connection closed)");
+        else await SendMessageToAllAsync(Context.ConnectionId, $"Disconnected ({exception.Message})");
+    }
+
+    private async Task SendMessageToAllAsync(string user, string message)
+    {
+        _messageHistory.Add((user, message));
+        await Clients.All.SendAsync("ReceiveMessage", Context.ConnectionId, message);
+    }
+}
--- Blazor.WebApp/Blazor.WebApp/Program.cs
+++ Blazor.WebApp/Blazor.WebApp/Program.cs
@@ -1,14 +1,20 @@
 using Blazor.WebApp.Client.Pages;
 using Blazor.WebApp.Components;
+using Blazor.WebApp.Hubs;
+using Microsoft.AspNetCore.ResponseCompression;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddMudServices();
 builder.Services.AddRazorComponents()
     .AddInteractiveWebAssemblyComponents();
+builder.Services.AddSignalR();
+builder.Services.AddResponseCompression(options => 
+    options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(["application/octet-stream"]));
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -16,6 +22,7 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
+    app.UseResponseCompression();
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
@@ -30,4 +37,5 @@ app.MapRazorComponents<App>()
     .AddInteractiveWebAssemblyRenderMode()
     .AddAdditionalAssemblies(typeof(Blazor.WebApp.Client._Imports).Assembly);
 
+app.MapHub<CombatHub>("/combathub");
 app.Run();
